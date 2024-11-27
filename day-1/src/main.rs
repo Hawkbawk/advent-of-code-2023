f9deb41b11b620dffc18084d73a2db0c527a72bf
Ryan Hawkins <ryanlarryhawkins@gmail.com>
2024-11-26 23:47:47 -0700

    initial attempt at part two

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,8 +1,12 @@
 use std::fs::{self};
 
+use regex::Regex;
+
 fn main() {
     println!("The answer for the first half follows:");
     part_one();
+    println!("The answer for the second half follows:");
+    part_two();
 }
 
 fn part_one() {
@@ -21,4 +25,39 @@ fn part_one() {
         });
 
     println!("The sum is: {result}");
+}
+
+fn part_two() {
+    let pattern = Regex::new("one|two|three|four|five|six|seven|eight|nine|\\d").unwrap();
+    let result = fs::read_to_string("puzzle.txt")
+        .unwrap()
+        .lines()
+        .filter_map(|line| {
+            if line.trim().is_empty() {
+                return None;
+            }
+
+            Some(
+                pattern
+                    .find_iter(line)
+                    .map(|m| match m.as_str() {
+                        "one" => 1,
+                        "two" => 2,
+                        "three" => 3,
+                        "four" => 4,
+                        "five" => 5,
+                        "six" => 6,
+                        "seven" => 7,
+                        "eight" => 8,
+                        "nine" => 9,
+                        _ => m.as_str().parse().unwrap(),
+                    })
+                    .collect::<Vec<i32>>(),
+            )
+        })
+        .fold(0, |acc, digits| {
+            acc + digits.first().unwrap() * 10 + digits.last().unwrap()
+        });
+
+    println!("The result after considering spelled out digits is: {result}");
 }
