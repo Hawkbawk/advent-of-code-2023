056789234637b77e5e2f6c39503d7f4a1723405d
Ryan Hawkins <ryanlarryhawkins@gmail.com>
2024-12-15 14:29:26 -0700

    Day 2 Part Two
    This completes part two for day 2.
    It was pretty easy, just had to refactor some of my code to split the
    game parsing functionality into it's own function. Easy enough.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -2,74 +2,113 @@ use std::fs;
 
 use regex::Regex;
 
-const NUM_RED: u32 = 12;
-const NUM_GREEN: u32 = 13;
-const NUM_BLUE: u32 = 14;
+const NUM_RED: u64 = 12;
+const NUM_GREEN: u64 = 13;
+const NUM_BLUE: u64 = 14;
 
 fn main() {
     env_logger::init();
     let input = fs::read_to_string("puzzle.txt").expect("Couldn't find the puzzle input");
     let result = part_one(&input, NUM_RED, NUM_GREEN, NUM_BLUE);
-    println!("The sum of all valid games is {}", result)
+    println!("The sum of the id's of all valid games is {result}");
+
+    let result = part_two(&input);
+
+    println!("The answer to part two is: {result}")
 }
 
 struct Game {
-    id: u32,
+    id: u64,
     sets: Vec<CubeSet>,
 }
 
 #[derive(Debug)]
 struct CubeSet {
-    num_red: u32,
-    num_blue: u32,
-    num_green: u32,
+    num_red: u64,
+    num_blue: u64,
+    num_green: u64,
 }
 
-fn part_one(input: &String, num_red: u32, num_green: u32, num_blue: u32) -> u32 {
+fn create_games(input: &String) -> Vec<Game> {
     let red_count_regex = Regex::new(r"(\d+) red").unwrap();
     let green_count_regex = Regex::new(r"(\d+) green").unwrap();
     let blue_count_regex = Regex::new(r"(\d+) blue").unwrap();
-
     let game_num_regex = Regex::new(r"Game (\d+)").unwrap();
-    let games = input.lines().map(|line| {
-        let game_num = game_num_regex
-            .captures(line)
-            .unwrap()
-            .get(1)
-            .unwrap()
-            .as_str()
-            .parse::<u32>()
-            .unwrap();
-        let sets = line.split(";").collect::<Vec<&str>>();
-        log::debug!("Parsing game {game_num}");
-        let sets = sets
-            .iter()
-            .map(|set| {
-                let num_red = match red_count_regex.captures(set) {
-                    Some(captures) => captures[1].parse::<u32>().unwrap(),
-                    None => 0,
-                };
-                let num_blue = match blue_count_regex.captures(set) {
-                    Some(captures) => captures[1].parse::<u32>().unwrap(),
-                    None => 0,
-                };
-                let num_green = match green_count_regex.captures(set) {
-                    Some(captures) => captures[1].parse::<u32>().unwrap(),
-                    None => 0,
-                };
-                CubeSet {
-                    num_red,
-                    num_blue,
-                    num_green,
-                }
-            })
-            .collect::<Vec<CubeSet>>();
-        log::debug!("Sets are as follows: {:?}", sets);
 
-        Game { id: game_num, sets }
+    input
+        .lines()
+        .map(|line| {
+            let game_num = game_num_regex
+                .captures(line)
+                .unwrap()
+                .get(1)
+                .unwrap()
+                .as_str()
+                .parse::<u64>()
+                .unwrap();
+            let sets = line.split(";").collect::<Vec<&str>>();
+            log::debug!("Parsing game {game_num}");
+            let sets = sets
+                .iter()
+                .map(|set| {
+                    let num_red = match red_count_regex.captures(set) {
+                        Some(captures) => captures[1].parse::<u64>().unwrap(),
+                        None => 0,
+                    };
+                    let num_blue = match blue_count_regex.captures(set) {
+                        Some(captures) => captures[1].parse::<u64>().unwrap(),
+                        None => 0,
+                    };
+                    let num_green = match green_count_regex.captures(set) {
+                        Some(captures) => captures[1].parse::<u64>().unwrap(),
+                        None => 0,
+                    };
+                    CubeSet {
+                        num_red,
+                        num_blue,
+                        num_green,
+                    }
+                })
+                .collect::<Vec<CubeSet>>();
+            log::debug!("Sets are as follows: {:?}", sets);
+
+            Game { id: game_num, sets }
+        })
+        .collect::<Vec<Game>>()
+}
+
+fn part_two(input: &String) -> u64 {
+    let games = create_games(input);
+
+    let cube_sets = games.iter().map(|game| {
+        let num_red = game.sets.iter().map(|s| s.num_red).max().unwrap();
+        let num_green = game.sets.iter().map(|s| s.num_green).max().unwrap();
+        let num_blue = game.sets.iter().map(|s| s.num_blue).max().unwrap();
+
+        log::debug!(
+            "For game {}, here are the max number of each cube needed:\nr: {}, g: {}, b: {}",
+            game.id,
+            num_red,
+            num_green,
+            num_blue
+        );
+
+        CubeSet {
+            num_red,
+            num_blue,
+            num_green,
+        }
     });
 
-    games.fold(0, |acc, game| {
+    cube_sets.fold(0, |acc, item| {
+        acc + item.num_red * item.num_green * item.num_blue
+    })
+}
+
+fn part_one(input: &String, num_red: u64, num_green: u64, num_blue: u64) -> u64 {
+    let games = create_games(input);
+
+    games.iter().fold(0, |acc, game| {
         let invalid = game
             .sets
             .iter()
--- day-2/src/test.rs
+++ day-2/src/test.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 #[test]
-fn test_simple_example() {
+fn test_part_one_simple_example() {
     let input = "Game 1: 22 green; 11 blue; 19 red
 Game 2: 1 red; 3 green, 2 blue; 9 green";
     let result = part_one(&input.to_string(), NUM_RED, NUM_GREEN, NUM_BLUE);
@@ -9,7 +9,7 @@ Game 2: 1 red; 3 green, 2 blue; 9 green";
 }
 
 #[test]
-fn test_given_example() {
+fn test_part_one_given_example() {
     env_logger::init();
     let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -20,4 +20,21 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
         part_one(&input.to_string(), NUM_RED, NUM_GREEN, NUM_BLUE),
         8
     );
+}
+
+#[test]
+fn test_part_two_simple_example() {
+    let input = "Game 1: 11 green, 2 blue; 20 blue; 19 red
+Game 2: 4 green, 2 blue";
+    assert_eq!(part_two(&input.to_string()), 4180);
+}
+
+#[test]
+fn test_part_two_given_example() {
+    let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    assert_eq!(part_two(&input.to_string()), 2286);
 }
